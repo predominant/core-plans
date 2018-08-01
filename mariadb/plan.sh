135968939c64a89f59a91cef81b3b9bbead0e7cd
Graham Weldon <graham@grahamweldon.com>
2018-08-01 17:28:03 +0900

    Upgrade to MariaDB 10.3.8
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- mariadb/plan.sh
+++ mariadb/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=mariadb
 pkg_origin=core
-pkg_version=10.1.24
+pkg_version=10.3.8
 pkg_description="An open source monitoring software for networks and applications"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_upstream_url="https://mariadb.org/"
 pkg_license=('GPL-2.0')
 pkg_source="http://ftp.hosteurope.de/mirror/archive.mariadb.org//${pkg_name}-${pkg_version}/source/${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="b3df99ae5b1ec8cf6cede4cbc4ae3f54ce66464549cba6d56d9ff4d24e4d551e"
+pkg_shasum="30bec17514d12d811a05757be46bd41724df28002cdec550d5c757b0b3f31ab8"
 pkg_deps=(
   core/gcc-libs
   core/ncurses
@@ -17,6 +17,10 @@ pkg_build_deps=(
   core/make
   core/coreutils
   core/cmake
+  core/gnutls
+  core/pkg-config
+  core/bison
+  core/m4
 )
 pkg_bin_dirs=(bin)
 pkg_include_dirs=(include)
@@ -25,29 +29,31 @@ pkg_exports=(
   [port]=port
 )
 pkg_exposes=(port)
-pkg_svc_user="hab"
 
 do_prepare() {
-    if [ -f CMakeCache.txt ]; then
-      rm CMakeCache.txt
-    fi
+  if [ -f CMakeCache.txt ]; then
+    rm CMakeCache.txt
+  fi
 
-    sed -i 's/^.*abi_check.*$/#/' CMakeLists.txt
-    sed -i "s@data/test@\${INSTALL_MYSQLTESTDIR}@g" sql/CMakeLists.txt
-    export CXXFLAGS="$CFLAGS"
+  sed -i 's/^.*abi_check.*$/#/' CMakeLists.txt
+  sed -i "s@data/test@\${INSTALL_MYSQLTESTDIR}@g" sql/CMakeLists.txt
+  export CXXFLAGS="$CFLAGS"
 }
 
 do_build() {
-    cmake . -DCMAKE_INSTALL_PREFIX="${pkg_prefix}" \
-            -DCMAKE_PREFIX_PATH="$(pkg_path_for core/ncurses)" \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DWITH_READLINE=OFF
-    make
+  cmake . \
+    -DCMAKE_INSTALL_PREFIX="${pkg_prefix}" \
+    -DCMAKE_PREFIX_PATH="$(pkg_path_for core/ncurses)" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_READLINE=OFF \
+    -DGNUTLS_LIBRARY="$(pkg_path_for core/gnutls)/lib" \
+    -DGNUTLS_INCLUDE_DIR="$(pkg_path_for core/gnutls)/include"
+  make
 }
 
 do_install() {
-    make install
-    rm -rf "${pkg_prefix}/mysql-test"
-    rm -rf "${pkg_prefix}/bin/mysql_client_test"
-    rm -rf "${pkg_prefix}/bin/mysql_test"
+  make install
+  rm -rf "${pkg_prefix}/mysql-test"
+  rm -rf "${pkg_prefix}/bin/mysql_client_test"
+  rm -rf "${pkg_prefix}/bin/mysql_test"
 }
