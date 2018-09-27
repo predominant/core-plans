191b52051e7b5a185c696603a85cb7876da8088c
David Somers-Harris <david@somers-harris.com>
2018-09-27 23:43:29 +0900

    Rename to haproxy16
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    Add README (partial draft)
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    Bumped minor version to current latest of 1.6
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    added haproxy17
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    added haproxy18
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    add general plan
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    make haproxy18 source haproxy
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    make haproxy17 source haproxy
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    make haproxy16 source haproxy
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    fix haproxy16 pkg dir
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    fix haproxy17 pkg dir
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    fix haproxy18 pkg dir
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    Updated READMEs
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    added haproxy16~18 to .bldr.toml
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>
    
    Added tests
    
    Signed-off-by: David Somers-Harris <david@somers-harris.com>

---
+++ haproxy16/plan.sh
@@ -0,0 +1,8 @@
+source ../haproxy/plan.sh
+
+pkg_name=haproxy16
+pkg_distname=haproxy
+pkg_version=1.6.14
+pkg_source=https://www.haproxy.org/download/1.6/src/${pkg_distname}-${pkg_version}.tar.gz
+pkg_shasum=bac949838a3a497221d1a9e937d60cba32156783a216146a524ce40675b6b828
+pkg_dirname="${pkg_distname}-${pkg_version}"
