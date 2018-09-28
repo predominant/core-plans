5e28f7635cbe2775889e7a0b9cb0c4851b55065b
David Somers-Harris <david@somers-harris.com>
2018-09-28 14:08:03 +0900

    make haproxy16 source haproxy
    Signed-off-by: David Somers-Harris <david@somers-harris.com>

--- haproxy16/plan.sh
+++ haproxy16/plan.sh
@@ -1,43 +1,6 @@
-pkg_origin=core
-pkg_name=haproxy
-pkg_description="The Reliable, High Performance TCP/HTTP Load Balancer"
+source ../haproxy/plan.sh
+
+pkg_name=haproxy16
 pkg_version=1.6.14
-pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
-pkg_license=('GPL-2.0' 'LGPL-2.1')
-pkg_source=http://www.haproxy.org/download/1.6/src/haproxy-${pkg_version}.tar.gz
-pkg_upstream_url="http://git.haproxy.org/git/haproxy-1.6.git/"
+pkg_source=https://www.haproxy.org/download/1.6/src/haproxy-${pkg_version}.tar.gz
 pkg_shasum=bac949838a3a497221d1a9e937d60cba32156783a216146a524ce40675b6b828
-pkg_svc_run='haproxy -f config/haproxy.conf -db'
-pkg_exports=(
-  [port]=front-end.port
-  [status-port]=status.port
-)
-pkg_exposes=(port status-port)
-pkg_binds=(
-  [backend]="port"
-)
-pkg_deps=(core/zlib core/pcre core/openssl)
-pkg_build_deps=(
-  core/coreutils
-  core/gcc
-  core/pcre
-  core/make
-  core/openssl
-  core/zlib
-)
-pkg_bin_dirs=(bin)
-
-do_build() {
-  make USE_PCRE=1 \
-       USE_PCRE_JIT=1 \
-       TARGET=linux2628 \
-       USE_OPENSSL=1 \
-       USE_ZLIB=1 \
-       ADDINC="$CFLAGS" \
-       ADDLIB="$LDFLAGS"
-}
-
-do_install() {
-  mkdir -p "$pkg_prefix"/bin
-  cp haproxy "$pkg_prefix"/bin
-}
