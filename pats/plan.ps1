5c9b2252737a37b2fa680d8a53e2246e14a4926e
Graham Weldon <graham@grahamweldon.com>
2019-03-29 16:39:13 +0900

    ?
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- pats/plan.ps1
+++ pats/plan.ps1
@@ -7,30 +7,19 @@ $pkg_description="PowerShell Acceptance Testing System"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://github.com/declarativesystems/pats/archive/master.zip"
 $pkg_shasum="8dd5b2a738e364d0ab14a5e99e2e9228eea2ce5a92645cbc322131e0a4e23aa4"
+$pkg_bin_dirs=@("bin")
 
-# pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-# pkg_shasum="TODO"
-# pkg_deps=(core/glibc)
-# pkg_build_deps=(core/make core/gcc)
-# pkg_lib_dirs=(lib)
-# pkg_include_dirs=(include)
-# pkg_bin_dirs=(bin)
-# pkg_pconfig_dirs=(lib/pconfig)
-# pkg_svc_run="haproxy -f $pkg_svc_config_path/haproxy.conf"
-# pkg_exports=(
-#   [host]=srv.address
-#   [port]=srv.port
-#   [ssl-port]=srv.ssl.port
-# )
-# pkg_exposes=(port ssl-port)
-# pkg_binds=(
-#   [database]="port host"
-# )
-# pkg_binds_optional=(
-#   [storage]="port host"
-# )
-# pkg_interpreters=(bin/bash)
-# pkg_svc_user="hab"
-# pkg_svc_group="$pkg_svc_user"
-# pkg_description="Some description."
-# pkg_upstream_url="http://example.com/project-name"
+# function Invoke-Unpack {
+#   mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+
+#   Push-Location "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+#   Pop-Location
+# }
+
+function Invoke-Install {
+  Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/node-v$pkg_version-x64/SourceDir/nodejs/*" "$pkg_prefix/bin" -Recurse
+}
+
+function Invoke-Check() {
+  (& "$HAB_CACHE_SRC_PATH/$pkg_dirname/nodejs/node" --version) -eq "v$pkg_version"
+}
