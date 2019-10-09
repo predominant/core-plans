44d3b32edf7198d70b950eeb240aa3b7fd64ab98
Graham Weldon <graham@grahamweldon.com>
2019-10-09 10:02:19 +0900

    WIP: Caddy 2.0.0-beta4 preliminary work
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- caddy/plan.sh
+++ caddy/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=caddy
 pkg_origin=core
-pkg_version=1.0.1
-pkg_maintainer='The Habitat Maintainers <humans@habitat.sh>'
+pkg_version=2.0.0-beta4
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
-pkg_source="https://github.com/caddyserver/caddy/releases/download/v${pkg_version}/caddy_v${pkg_version}_linux_amd64.tar.gz"
-pkg_shasum=4fd3ad67078c6b16e01ed2443bc8d7abf784a757de4177607e3cad2595db3caf
+pkg_source="https://github.com/caddyserver/caddy/releases/download/v${pkg_version}/caddy2_beta4_linux_amd64"
+pkg_shasum=a172bcc398852cf7e9cd2c92f5c6220c52738edc89d2c4ae9a9816c074ad33d8
 pkg_description="Fast, cross-platform HTTP/2 web server with automatic HTTPS"
 pkg_upstream_url=https://caddyserver.com
-pkg_svc_run="caddy -conf ${pkg_svc_config_path}/Caddyfile"
+pkg_svc_run="caddy run --config ${pkg_svc_config_path}/Caddyfile --adapter caddyfile"
 pkg_exposes=(port)
 pkg_exports=(
   [port]=http.port
@@ -16,10 +16,14 @@ pkg_bin_dirs=(bin)
 pkg_deps=(core/glibc)
 pkg_dirname="${pkg_name}-v${pkg_version}"
 
+do_unpack() {
+  return 0
+}
+
 do_build() {
   return 0
 }
 
 do_install() {
-  install -Dm755 "${HAB_CACHE_SRC_PATH}/caddy" "${pkg_prefix}/bin/caddy"
+  install -Dm755 "${HAB_CACHE_SRC_PATH}/caddy2_beta4_linux_amd64" "${pkg_prefix}/bin/caddy"
 }
